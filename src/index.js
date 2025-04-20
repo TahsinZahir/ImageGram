e474fee23b8a2165c868cb0431d2938dac833dc2
TahsinZahir <tahsindbg0142@gmail.com>
2025-04-20 23:50:47 +0530

    Added middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
+import { createPost } from './controllers/postController.js';
 
-const PORT=3099;
+const PORT=3000;
 
 const app=express();//create express app server instance
 
-//create a route for the app
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
 
 app.get('/ping',(req,res)=>{
+    console.log(req.query);
+    console.log(req.body);
 return res.json({message:'pong'});
 });
+
+app.post('/posts', createPost);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
