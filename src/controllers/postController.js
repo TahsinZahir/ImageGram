9f4f8fd76eb5176462a0e8ed6cbdc4a7887ccc2d
TahsinZahir <tahsindbg0142@gmail.com>
2025-04-21 02:31:53 +0530

    Added code for uploading files to s3

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,6 @@
 
 export async function createPost(req, res) {
+    console.log(req.file);
     // call the service layer function
     return res.json({ message: 'Post created successfully' });
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import connectDB from './config/dbConfig.js';
 import { createPost } from './controllers/postController.js';
+import { s3uploader } from './config/multerConfig.js';
 
 const PORT=3000;
 
 const app=express();//create express app server instance
 
 app.use(express.json());
-app.use(express.text());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
+
+
 
 app.get('/ping',(req,res)=>{
     console.log(req.query);
@@ -16,7 +18,7 @@ app.get('/ping',(req,res)=>{
 return res.json({message:'pong'});
 });
 
-app.post('/posts', createPost);
+app.post('/posts', s3uploader.single('image'), createPost);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
